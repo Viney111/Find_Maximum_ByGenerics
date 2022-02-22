3cbf3c0277ecc08a73205892b91752b28b111e78
Viney <vineykhaneja999@gmail.com>
2022-02-22 15:40:02 +0530

    [Viney] FEATURE ADDED: Finding Maximum elements of Array

--- FindMaxByGeneric.cs
+++ FindMaxByGeneric.cs
@@ -8,22 +8,26 @@ namespace Find_Maximum_ByGenerics
 {
     public class FindMaxByGeneric <T>
     {
+        //FIELD
         public T[] arrayofElement;
-
+        //CONSTRUCTOR
         public FindMaxByGeneric(T[] arrayofElement)
         {
             this.arrayofElement = arrayofElement;
         }
+        //SORTING ELEMENTS & RETURNING IT IN ARRAY FORM
         public T[] Sorting(T[] arrayofElements)
         {
             Array.Sort(arrayofElements);
             return arrayofElements ;
         }
+        //RTURNING MAX VALUE BY CALLING SORTING METHOD INSIDE
         public T MaxarrayofElement( T[] arrayofElements)
         {
             var sorted_arrayofElements = Sorting(arrayofElements);
             return sorted_arrayofElements[^1];
         }
+        //FINDING MAX VALUE AMONG THE ARRAY WHICH WE ARE PASSING IN CONSTRUCTOR
         public T MaxMethod()
         {
             var max = MaxarrayofElement(arrayofElement);
--- Program.cs
+++ Program.cs
@@ -13,10 +13,13 @@ namespace FindMaximumByGenerics
             int[] arrayOfIntegers = { 1, 2, 3, 4, 5, 6 };
             float[] arrayOfFloats = { 5, 6, 7, 8, 9 };
             string[] arrayOfStrings = {"a", "b", "c", "d", "e", "f" };
-            FindMaxByGeneric <int> findMaxInt = new FindMaxByGeneric<int>(arrayOfIntegers);
+
+            //Making objects & passing arrays of different forms in the constructor of FindMaxByGeneric Class.
+            FindMaxByGeneric<int> findMaxInt = new FindMaxByGeneric<int>(arrayOfIntegers);
             FindMaxByGeneric<float>findMaxDouble = new FindMaxByGeneric<float>(arrayOfFloats);
             FindMaxByGeneric<string>findMaxString = new FindMaxByGeneric<string>(arrayOfStrings);
 
+            //Calling MaxMethod from FindMaxByGeneric Class.
             Console.WriteLine($"Max integer value is {findMaxInt.MaxMethod()}");
             Console.WriteLine($"Max Float value is {findMaxDouble.MaxMethod()}");
             Console.WriteLine($"Maximum string is {findMaxString.MaxMethod()}");
