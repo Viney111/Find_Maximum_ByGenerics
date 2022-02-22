14dc29b60b6e4fbb46e752d474904de444eac0d8
Viney <vineykhaneja999@gmail.com>
2022-02-22 13:09:12 +0530

    [Viney] FEATURE ADDED: Refactoring to Find Max by Generics Type Method

--- Program.cs
+++ Program.cs
@@ -10,21 +10,23 @@ namespace FindMaximumByGenerics
         static void Main(string[] args)
         {
             Program program = new Program();
-            Console.WriteLine($"Maximum String is {program.findMaxString("A","P","B")}");
+            Console.WriteLine($"Maximum integer in the list is {program.findMax<int>(10, 5, 6)}");
+            Console.WriteLine($"Maximum float in the list is {program.findMax<float>(4.5F, 5.9F, 6.9F)}");
+            Console.WriteLine($"Maximum string in the list is {program.findMax<string>("A", "Z", "F")}");
         }
-        public string findMaxString(string str1, string str2, string str3)
+        public T findMax<T>(T element1, T element2, T element3)
         {
-            if (str1.CompareTo(str2) > 0 && str1.CompareTo(str3) > 0)
+            if (Comparer<T>.Default.Compare(element1,element2) > 0 && Comparer<T>.Default.Compare(element1, element3) > 0)
             {
-                return str1;
+                return element1;
             }
-            else if (str2.CompareTo(str3) > 0 && str2.CompareTo(str1) > 0)
+            else if (Comparer<T>.Default.Compare(element2, element1) > 0 && Comparer<T>.Default.Compare(element2, element3) > 0)
             {
-                return str2;
+                return element2;
             }
             else
             {
-                return str3;  
+                return element3;  
             }
         }
     }
